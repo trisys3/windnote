89ad6bfd983be307d33026e86e23f734289b9638
Sean Moran <sean_r_moran@msn.com>
2017-05-29 03:29:39 -0400

    Added hot reloading to the outer React component, though the inner components still do not have this functionality

--- notes/app.js
+++ notes/app.js
@@ -1,24 +1,27 @@
 // connect to the home SocketIO namespace
+import {AppContainer} from 'react-hot-loader';
 import io from 'socket.io-client';
 const socket = io(__dirname);
 
 import React from 'react';
 import {render} from 'react-dom';
 
-import {MdParser} from './md-parser';
+import MdParser from './md-parser';
 import './app.css';
+
+const notesParser = document.querySelector('notes-parser');
+const reactReload = () => render(<AppContainer>
+  <MdParser />
+</AppContainer>, notesParser);
+
+reactReload();
 
 if(module.hot) {
   module.hot.accept();
 
-  module.hot.accept('./md-parser', () => {
-    const MdContainer = require('./md-parser').default;
-    render(<MdContainer />, document.querySelector('notes-parser'));
-  });
+  module.hot.accept('./md-parser', reactReload());
 
   socket.once('hot-update', () => {
     module.hot.check(true);
   });
 }
-
-render(<MdParser />, document.querySelector('notes-parser'));
